958793c1b399483e3bc7911d9ec3011a7fbc4505
rayfalmayvand <rayfalmayvand@gmail.com>
2026-03-10 11:46:17 +0700

    Add files via upload
    diceeapp

--- main.dart
+++ main.dart
@@ -1,112 +1,63 @@
 import 'package:flutter/material.dart';
-import 'package:google_fonts/google_fonts.dart';
+import 'dart:math';
 
 void main() {
-  runApp(const MyApp());
+  runApp(
+    MaterialApp(
+      home: Scaffold(
+        backgroundColor: Colors.amber,
+        appBar: AppBar(
+          title: Text(
+            "Dicee App",
+            style: TextStyle(color: Colors.white),
+          ),
+          backgroundColor: Colors.deepOrange,
+        ),
+        body: MyApp(),
+      ),
+    ),
+  );
 }
 
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
+class MyApp extends StatefulWidget {
+  const MyApp ({super.key});
+
+  @override
+  State<MyApp> createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  int leftDiceNumber = 1;
+  int rightDiceNumber = 1;
+
+  void changeDiceFace() {
+    setState(() {
+      leftDiceNumber = Random().nextInt(6) + 1;
+      rightDiceNumber = Random().nextInt(6) + 1;
+    });
+  }
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      home: Scaffold(
-        backgroundColor: Colors.lightBlueAccent,
-        body: SafeArea(
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-              CircleAvatar(
-                radius: 50,
-                backgroundImage: AssetImage(
-                  "images/profileme.png",
-                ),
-              ),
-              Text(
-                "Sabil Casablancas",
-                style: GoogleFonts.pacifico(
-                  fontSize: 45,
-                  color: Colors.white,
-                  fontWeight: FontWeight.bold,
-                ),
-              ),
-              Text(
-                "Professional Android Developer",
-                style: GoogleFonts.padauk(
-                  fontSize: 15,
-                  color: Colors.blueGrey,
-                  fontWeight: FontWeight.bold,
-                  letterSpacing: 2.5,
-                ),
-              ),
-              SizedBox(
-                height: 20,
-                width: 145,
-                child: Divider(
-                  color: Colors.indigo,
-                ),
-              ),
-              Container(
-                padding: EdgeInsets.all(10),
-                decoration: BoxDecoration(
-                  color: Colors.white,
-                  borderRadius: BorderRadius.circular(15),
-                ),
-                margin: EdgeInsets.symmetric(
-                  vertical: 10,
-                  horizontal: 25,
-                ),
-                child: Row(
-                  children: [
-                    Icon(
-                      Icons.phone,
-                      color: Colors.indigo,
-                    ),
-                    SizedBox(
-                      width: 15,
-                    ),
-                    Text(
-                      "081286149724",
-                      style: GoogleFonts.sourceSerif4(
-                        fontSize: 20,
-                        color: Colors.indigo,
-                      ),
-                    ),
-                  ],
-                ),
-              ),
-              Container(
-                padding: EdgeInsets.all(10),
-                decoration: BoxDecoration(
-                  color: Colors.white,
-                  borderRadius: BorderRadius.circular(15),
-                ),
-                margin: EdgeInsets.symmetric(
-                  vertical: 10,
-                  horizontal: 25,
-                ),
-                child: Row(
-                  children: [
-                    Icon(
-                      Icons.email,
-                      color: Colors.indigo,
-                    ),
-                    SizedBox(
-                      width: 10,
-                    ),
-                    Text(
-                      "sabielcasab@mail.com",
-                      style: GoogleFonts.aBeeZee(
-                        fontSize: 20, color: Colors.indigo
-                      ),
-                    ),
-                  ],
-                ),
-              )
-            ],
+    return Center(
+      child: Row(
+        children: [
+          Expanded(
+            child: TextButton(
+              onPressed: () {
+                  changeDiceFace();
+              },
+              child: Image.asset('images/dice$leftDiceNumber.png'),
+            ),
           ),
-        ),
+          Expanded(
+            child: TextButton(
+              onPressed: () {
+                  changeDiceFace();
+              },
+              child: Image.asset('images/dice$rightDiceNumber.png')),
+          ),
+        ],
       ),
     );
   }
